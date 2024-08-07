103829d22aa8c222d849cb3473d16e18eca738e8
EdwardThapelo <edwardthapelo55@gmail.com>
2024-08-07 16:13:46 +0200

    Made a search fuction for the Api

---
+++ web-app/README.md
@@ -0,0 +1,8 @@
+# React Weather app
+
+# For Usage
+You can have a profile, or visit the app without a profile
+as a Guest
+
+# What it does
+
---
+++ web-app/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b1b5d2a308750daddd759c02fe1ed6d9`;
+
+  const searchLocation = (e) => {
+    if (e.key === "Enter") {
+      axios.get(url).then((Response) => {
+        setData(Response.data);
+        console.log(Response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder="Search for a location"
+          onKeyPress={searchLocation}
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Soweto</p>
+          </div>
+          <div className="temp">
+            <h1>60*F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">60*F</p>
+            <p>Temperature</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">44km/h</p>
+            <p>Wind speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
